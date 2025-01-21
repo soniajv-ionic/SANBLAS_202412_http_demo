13fc492542dba20665dd167e1c09e6e63971bdc9
soniajv-ionic <soniajv.ionic@gmail.com>
2025-01-21 12:13:16 +0100

    update 21/01/2025

--- src/app/components/busqueda-museo/busqueda-museo.component.ts
+++ src/app/components/busqueda-museo/busqueda-museo.component.ts
@@ -1,13 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { JuntaAndaluciaService } from '../../services/junta-andalucia.service';
 import { IInstalacionMuseistica } from '../../interfaces/iinstalacion-museistica';
-import { HttpErrorResponse } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
+import { HttpErrorResponse } from '@angular/common/http';
 
 
 @Component({
   selector: 'app-museo',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './busqueda-museo.component.html',
   styleUrls: ['./busqueda-museo.component.css']
 })
@@ -16,20 +18,36 @@ export class MuseoComponent {
   museos: IInstalacionMuseistica[] = []; // Lista original de museos
   museosFiltrados: IInstalacionMuseistica[] = []; // Lista de museos filtrados
   provinciaBuscada: string = ''; // Provincia que se está buscando
+   private juntaAndaluciaService = inject(JuntaAndaluciaService);
 
   // Método para cargar los museos (simulado con datos estáticos)
   cargarDatos() {
     this.cargando = true;
 
     // Simulación de carga de datos (puedes reemplazar esto con una llamada HTTP real)
-    setTimeout(() => {
-      this.museos = [
-      ];
+    this.cargando = true;
+    this.juntaAndaluciaService.getMuseos().subscribe(
+      {
+        next: (retorno: IInstalacionMuseistica[]) => {
+          this.museos = retorno;
+        },
+        error: (error: HttpErrorResponse) => {
+          //error es de tipo HttpErrorResponse
+          console.log("Error name :" + error.name);
+          console.log("Error message:" + error.message);
+          console.log("Error error:" + error.error);
+          console.log("Error ok:" + error.ok);
+        },
+        complete: () => {
+          console.log("Complete");
+          this.cargando = false;
+        }
+      });
 
       // Filtrar museos según la provincia cuando se cargan los datos
       this.filtrarMuseos();
       this.cargando = false;
-    }, 1000);
+
   }
 
   // Método para filtrar los museos por provincia
